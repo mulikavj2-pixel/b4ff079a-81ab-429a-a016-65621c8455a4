a2cde7791655b345e3af28efebb5b119f5c8487f
mulikavj2-pixel <mulikavj2@gmail.com>
2025-12-16 16:22:39 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { ThemeProvider } from "@/providers/themeProvider/ThemeProvider";
 import NavbarStyleFullscreen from '@/components/navbar/NavbarStyleFullscreen/NavbarStyleFullscreen';
